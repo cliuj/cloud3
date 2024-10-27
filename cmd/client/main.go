dc7bd8b03ded4dd394642fa4ae69c7785b4c294d
cliuj <jliu2179@gmail.com>
2024-10-27 19:31:55 -0400

    feat: Add gin server to client and server
    * Updated corresponding docker-compose entries

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	CLIENT_PORT = os.Getenv("CLIENT_PORT")
 )
 
 func main() {
 	fmt.Println("Hello world")
+
+	if CLIENT_PORT == "" {
+		CLIENT_PORT = "8080"
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":" + CLIENT_PORT)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	SERVER_PORT = os.Getenv("SERVER_PORT")
 )
 
 
 func main() {
 	fmt.Println("Hello world")
+	if SERVER_PORT == "" {
+		SERVER_PORT = "8000"
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":" + SERVER_PORT)
 }
