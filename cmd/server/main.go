e5a666506666f343a4d93a1fb3a9b16f53814ce3
cliuj <jliu2179@gmail.com>
2024-10-27 20:22:31 -0400

    feat(server): Add upload route for server to receive
    Allows file uploading via the `/files/upload` route. Destination is
    pre-set to the SHARED_DIR

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"path"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,21 +14,39 @@ var (
 	SHARED_DIR = os.Getenv("SHARED_DIR")
 )
 
+func loadENVs() {
+	if SERVER_PORT == "" {
+		SERVER_PORT = "8080"
+	}
+
+	if SHARED_DIR == "" {
+		SHARED_DIR = "/tmp/cloud3/client"
+	}
+
+}
 
 func main() {
 	fmt.Println("Hello world")
-	if SERVER_PORT == "" {
-		SERVER_PORT = "8000"
-	}
-	if SHARED_DIR == "" {
-		SHARED_DIR = "/tmp/cloud3"
-	}
+	loadENVs()
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	
+	r.POST("/files/upload", func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File["upload[]"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+
+			dst := path.Join(SHARED_DIR, file.Filename)
+			c.SaveUploadedFile(file, dst)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 	})
 
 	r.Run(":" + SERVER_PORT)
